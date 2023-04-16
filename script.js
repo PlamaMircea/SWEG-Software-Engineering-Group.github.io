e96206e7140828a8e3b4fbe0518900dc1c46ece0
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-04-16 09:57:37 +0200

    Menù con jQuery

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-    document.querySelector('.dropdown-link').addEventListener('click', function() {
-        var dropdownContent = document.querySelector('.dropdown-content');
-        dropdownContent.classList.toggle('show');
+$(document).ready(function(){
+    $('.accordion-header').click(function(){
+      $(this).toggleClass('active');
+      $(this).next('.accordion-content').slideToggle();
     });
-    
-    var accordionHeaders = document.querySelectorAll('.accordion-header');
-    
-    for (var i = 0; i < accordionHeaders.length; i++) {
-        accordionHeaders[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-        var accordionContent = this.nextElementSibling;
-        
-        if (accordionContent.style.display === 'block') {
-            accordionContent.style.display = 'none';
-        } else {
-            accordionContent.style.display = 'block';
-        }
-    });
-}
+  });
+  
