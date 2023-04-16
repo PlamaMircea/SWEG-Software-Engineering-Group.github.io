f5c49cd4e0bcdc0f2108373b7387be1c4dbb9279
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-04-16 09:29:47 +0200

    Link alle sottosezioni

---
+++ script.js
@@ -0,0 +1,18 @@
+  document.getElementsByClassName("dropbtn")[0].onclick = function() {
+    document.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
+  }
+
+  /* Chiude la sezione se si clicca fuori dalla sezione */
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
