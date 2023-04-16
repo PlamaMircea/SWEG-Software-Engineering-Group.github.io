80ed6fa347c7bf2d1bbd8458ff95ae03a660c100
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-04-16 10:26:39 +0200

    Other fixes

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-$(document).ready(function(){
-    $('.accordion-header').click(function(){
-      $(this).toggleClass('active');
-      $(this).next('.accordion-content').slideToggle();
-    });
+// seleziona tutti gli accordion-header
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+// aggiungi un event listener a ciascuno
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    // toggle della visualizzazione di content
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+    // aggiungi o rimuovi la classe accordion-active all'header
+    header.classList.toggle('accordion-content');
   });
-  
+});
